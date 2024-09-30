a030c04a40b28a173c7dbf6f968de09d44a44c88
himawan.masyaid <himawan.masyaid@gmail.com>
2024-09-30 20:54:33 +0700

    create : headline adapter

--- app/src/main/java/com/hmwn/headlinenewsmaker/view/main/HeadlineAdapter.kt
+++ app/src/main/java/com/hmwn/headlinenewsmaker/view/main/HeadlineAdapter.kt
@@ -1,4 +1,59 @@
 package com.hmwn.headlinenewsmaker.view.main
 
-class HeadlineAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hmwn.headlinenewsmaker.data.local.entity.HeadlineNewsEntity
+import com.hmwn.headlinenewsmaker.databinding.ItemHeadlineHistoryBinding
+
+class HeadlineAdapter : RecyclerView.Adapter<HeadlineAdapter.ViewHolder>() {
+
+    var listData: MutableList<HeadlineNewsEntity> = ArrayList()
+
+    fun insertAll(data: List<HeadlineNewsEntity>) {
+        data.forEach {
+            listData.add(it)
+            notifyItemInserted(listData.size - 1)
+        }
+    }
+
+    fun clear() {
+        if (listData.isNotEmpty()) {
+            listData.clear()
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemHeadlineHistoryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listData[position]
+        holder.bindTo(item)
+    }
+
+    override fun getItemCount() = listData.size
+
+    inner class ViewHolder(val binding: ItemHeadlineHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindTo(data: HeadlineNewsEntity) {
+
+            with(binding) {
+
+                tvHeadline.text = data.headline
+
+            }
+
+        }
+
+    }
+
 }
